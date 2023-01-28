346531d17ede23ac91c45229757da57043e269e2
fiskryeziu <fiskryeziu@gmail.com>
2023-01-28 22:17:42 +0100

    putobjectcommand

--- middlewares/s3Service.js
+++ middlewares/s3Service.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid'
 import { config } from 'dotenv'
 import AWS from 'aws-sdk'
-import { S3Client } from '@aws-sdk/client-s3'
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 
 config()
 
